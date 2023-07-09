df2acde3a6e9f1861bde3d13cc833da6e5fa5096
anuj-jft <anuj.singh@jellyfishtechnologies.com>
2023-07-10 00:02:10 +0530

    login form  basic design

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import AboutScreen from './components/AboutScreen';
+import Login from './screen/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AboutScreen />
+      <Login />
       <StatusBar style="auto" />
     </View>
   );
